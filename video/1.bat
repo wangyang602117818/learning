ce949ddde2a82bba99570adaf34920f4f0cfd267
wangyang <602117818@qq.com>
2016-07-25 21:17:28 +0800

    sdf

--- video/1.bat
+++ video/1.bat
@@ -1,1 +1,1 @@
-ffmpeg -i output.mp4 -c:v libx264 -c:a aac -strict -2 -f hls -hls_list_size 0 -hls_time 5 output1.m3u8 
+ffmpeg -i 1.mp4 -c:v libx264 -c:a aac -strict -2 -f hls -hls_time 5 output.m3u8 
