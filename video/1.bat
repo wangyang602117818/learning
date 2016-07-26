9cb5ead5ac416a4a8d5cee76675e580e6bb170d0
wangyang <18518317472@163.com>
2016-07-26 16:13:31 +0800

    video

--- video/1.bat
+++ video/1.bat
@@ -1,1 +1,8 @@
-ffmpeg -i 1.mp4 -c:v libx264 -c:a aac -strict -2 -f hls -hls_time 20 output.m3u8 
+rem ffmpeg -i 1.mp4 -c:v libx264 -c:a aac -f hls -hls_time 5 output.m3u8 
+
+rem ffmpeg -i 1.mp4 -c copy -f segment -segment_list playlist.m3u8 -segment_time 10 output.ts
+
+ffmpeg -i 1.mp4 -c:v libx264 -c:a aac -preset ultrafast -flags -global_header -f hls -hls_time 5 -hls_wrap 5 output.m3u8
+
+pause
+
