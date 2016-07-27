f01e15b48f3fe4d071c670a9713d80b22d0b9a6e
wangyang <18518317472@163.com>
2016-07-27 18:59:22 +0800

    000

--- video/1.bat
+++ video/1.bat
@@ -1,8 +1,6 @@
 rem ffmpeg -i 1.mp4 -c:v libx264 -c:a aac -f hls -hls_time 5 output.m3u8 
 
-rem ffmpeg -i 1.mp4 -c copy -f segment -segment_list playlist.m3u8 -segment_time 10 output.ts
-
-ffmpeg -i 1.mp4 -c:v libx264 -c:a aac -preset ultrafast -flags -global_header -f hls -hls_time 5 -hls_wrap 5 output.m3u8
+ffmpeg -i 1.mp4 -use_localtime 1 -hls_segment_filename 'file-%Y%m%d-%s.ts' out.m3u8
 
 pause
 
