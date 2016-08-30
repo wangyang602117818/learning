588985cca0fbc960603de94f2b08c6b241f8810a
wangyang <18518317472@163.com>
2016-08-30 18:58:26 +0800

    0

--- video/1.bat
+++ video/1.bat
@@ -1,6 +1,9 @@
-rem ffmpeg -i 1.mp4 -c:v libx264 -c:a aac -f hls -hls_time 5 output.m3u8 
 
-ffmpeg -i 1.mp4 -use_localtime 1 -hls_segment_filename 'file-%Y%m%d-%s.ts' out.m3u8
+rem ffmpeg -i 1.mp4 -c:v libx264 -c:a aac -strict -2 -f hls output.m3u8 
+
+rem ffmpeg -i 1.mp4 -c:v libx264 -c:a aac -strict -2 -f hls -hls_list_size 0 -hls_time 5 output1.m3u8 
+
+ffmpeg -i 1.mp4 -c:v libx264 -c:a aac -strict -2 -f hls -hls_list_size 0 -hls_time 5 ts\output1.m3u8
 
 pause
 
