37dcac2658b12b857373c8bda88bcd7a13a0f4b8
Yang Xa Wang <60211781@qq.com>
2021-12-23 17:49:31 +0800

    bug

--- video/1.bat
+++
@@ -1,9 +0,0 @@
-
-rem ffmpeg -i 1.mp4 -c:v libx264 -c:a aac -strict -2 -f hls output.m3u8 
-
-rem ffmpeg -i 1.mp4 -c:v libx264 -c:a aac -strict -2 -f hls -hls_list_size 0 -hls_time 5 output1.m3u8 
-
-ffmpeg -i 1.mp4 -c:v libx264 -c:a aac -strict -2 -f hls -hls_list_size 0 -hls_time 5 ts\output1.m3u8
-
-pause
-
